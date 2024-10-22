57fc7ad46539e65d6e05e4a53acd15a15bf53009
Roopa <roopa.chinta111@gmail.com>
2024-10-22 15:31:49 +0530

    code update with comments

--- listdata.java
+++ listdata.java
@@ -1,6 +1,6 @@
 class Driver{
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Helloworld!");
+      System.out.println("Helloworld!");clea
 	}
 }
